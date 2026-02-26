148ff9a5bd3d0b8f8ffa1ab335b913f440aeea04
Илья <drtyuicode@gmail.com>
2026-02-26 18:15:08 +0500

    little

--- config.fish
+++ config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g fish_greeting ""
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
 
 starship init fish | source
